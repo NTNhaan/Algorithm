ee40fd2b49760d411fa338ea14a1b7ffb6d5073a
NTNhaan <thanhnhannguyen30082003@gmail.com>
2024-08-12 20:34:56 +0700

    feat: add 15. 3Sum

---
+++ 15. 3Sum/15.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> ThreeSum(vector<int> nums)
+{
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        int j = i + 1;
+        int k = nums.size() - 1;
+        while (j < k)
+        {
+            int ans = nums[i] + nums[j] + nums[k];
+            if (ans > 0)
+            {
+                k--;
+            }
+            else if (ans < 0)
+            {
+                j++;
+            }
+            else
+            {
+                res.push_back({nums[i], nums[j], nums[k]});
+                j++;
+                while (nums[j] == nums[j - 1] && j < k)
+                {
+                    j++;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    vector<vector<int>> res = ThreeSum(nums);
+    for (int i = 0; i < res.size(); i++)
+    {
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
