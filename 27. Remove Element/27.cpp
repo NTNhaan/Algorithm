8317b9da4f7dd01ae9801eb7c1082a863edc8f07
NTNhaan <thanhnhannguyen30082003@gmail.com>
2024-08-14 20:27:48 +0700

    feat: add 27. Remove Element

---
+++ 27. Remove Element/27.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int RemoveElement(vector<int> nums, int val)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == val)
+        {
+            nums.erase(nums.begin() + i);
+            i--;
+        }
+    }
+    return nums.size();
+}
+int main()
+{
+    vector<int> nums;
+    int n, val;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    cin >> val;
+    RemoveElement(nums, val);
+    return 0;
+}
