61c97db465008496c2731128533d8cb33a55c312
NTNhaan <thanhnhannguyen30082003@gmail.com>
2025-02-03 14:40:42 +0700

    Bai 5 nay kho hieu qua chưa ac duoc

--- 5. Longest Palindromic Substring/5.cpp
+++ 5. Longest Palindromic Substring/5.cpp
@@ -1,8 +1,54 @@
-#include<iostream>
-#include<algorithm>
-#include<unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
+// khó hiểu quá chưa ac được
+string longestPalindrome(string s)
+{
+    int n = s.size();
+    if (n == 0)
+        return "";
 
-int main(){
-    
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int start = 0, maxLength = 1;
+
+    for (int i = 0; i < n; ++i)
+    {
+        dp[i][i] = true;
+    }
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (s[i] == s[i + 1])
+        {
+            dp[i][i + 1] = true;
+            start = i;
+            maxLength = 2;
+        }
+    }
+
+    for (int len = 3; len <= n; ++len)
+    {
+        for (int i = 0; i < n - len + 1; ++i)
+        {
+            int j = i + len - 1;
+            if (dp[i + 1][j - 1] && s[i] == s[j])
+            {
+                dp[i][j] = true;
+                start = i;
+                maxLength = len;
+            }
+        }
+    }
+
+    return s.substr(start, maxLength);
+}
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << longestPalindrome(s);
+    return 0;
 }
