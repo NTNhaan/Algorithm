eec72d352cc2514ab04ca00ba2f268a3bd5ae51f
NTNhaan <thanhnhannguyen30082003@gmail.com>
2025-02-06 22:27:31 +0700

    add problem 154. Find Minimum in Rotated Sorted Array II

---
+++ 154. Find Minimum in Rotated Sorted Array II/154.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMin(vector<int> &nums)
+{
+    int n = nums.size();
+    int tmp = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < tmp)
+        {
+            tmp = nums[i];
+        }
+    }
+    return tmp;
+}
+int anotherSolution(vector<int> &nums) // nếu sử dụng tìm ra số mid giữa left và right thì sẽ có trường hợp các dãy số trùng nhau.
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        nums[mid] < nums[right] ? right = mid : left = mid + 1;
+    }
+    return nums[left];
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> ans;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        ans.push_back(x);
+    }
+    cout << findMin(ans);
+    return 0;
+}
